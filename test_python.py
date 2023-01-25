67227206db511d7dfbb698f072b21aa3909f6403
la22badeamihaicristian <badea.mihai20073@gmail.com>
2023-01-25 17:58:36 +0200

    first commit

---
+++ test_python.py
@@ -0,0 +1,10 @@
+someNumber = 10
+
+if(someNumber >= 0):
+    print("Over zerro")
+elif(someNumber < 0):
+    print("Bellow zerro")
+else:
+    print("zerro")
+
+# Am inlocuit semnul "<" cu semnul ">" in linia 3 de cod astfel afisand rezultatul "Over zerro" la rularea programului.
